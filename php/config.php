872a1dc69d45054c2813096f53489870cbc13214
hycjs66 <1847686271@qq.com>
2019-03-21 17:38:20 +0800

    哎呦

--- php/config.php
+++ php/config.php
@@ -2,5 +2,12 @@
 @header("content-type:text/html;charset=utf8");
 mysql_connect("localhost:3306", "root", "root"); //你们的是3306
 mysql_select_db("1811"); //表示选择的执行的数据库
-mysql_query("set character set 'utf8'"); //设置执行语句的编码格式  防止中文乱码
- 
+mysql_query("set character set 'utf8'");
+
+
+
+// @header("content-type:text/html;charset=utf8");
+// mysql_connect("b-mmfrzcwiaxadnu.bch.rds.gz.baidubce.com:3306", "b_mmfrzcwiaxadnu", "123456"); //你们的是3306
+// mysql_select_db("b_mmfrzcwiaxadnu"); //表示选择的执行的数据库
+// mysql_query("set character set 'utf8'");
+ ?>
